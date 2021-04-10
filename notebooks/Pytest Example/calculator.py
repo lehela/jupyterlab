087df0b29bb8d2814cfef2069445aa6117ef711f
lehela <leif.lange@gmail.com>
2021-04-10 04:14:24 +0000

    Linted the Pytest examples

--- notebooks/Pytest Example/calculator.py
+++ notebooks/Pytest Example/calculator.py
@@ -1,32 +1,44 @@
+"""
+    Terrible calculator
+"""
+
 import numbers
 import sys
 
 class CalculatorError(Exception):
     """An exception class for calculator"""
-    
+
 class Calculator:
     """A terrible calculator"""
-    
-    def add(self, a, b):
-        self._check_operand(a)
-        self._check_operand(b)
-        return a + b
 
-    def subtract(self, a, b):
-        return a - b
+    def add(self, op_a, op_b):
+        """adds"""
+        self._check_operand(op_a)
+        self._check_operand(op_b)
+        return op_a + op_b
 
-    def multiply(self, a, b):
-        return a * b
+    @classmethod
+    def subtract(cls, op_a, op_b):
+        """subtracts"""
+        return op_a - op_b
 
-    def divide(self, a, b):
+    @classmethod
+    def multiply(cls, op_a, op_b):
+        """multiplies"""
+        return op_a * op_b
+
+    @classmethod
+    def divide(cls, op_a, op_b):
+        """divides"""
         try:
-            return a / b
-        except ZeroDivisionError:
-            raise CalculatorError("Division by zero is invalid")
-    
-    def _check_operand(self, operand):
+            return op_a / op_b
+        except ZeroDivisionError as exc_zero:
+            raise CalculatorError("Division by zero is invalid") from exc_zero
+
+    @classmethod
+    def _check_operand(cls, operand):
         if not isinstance(operand, numbers.Number):
-            raise CalculatorError(f'"{operand}" is not a number')
+            raise CalculatorError(f'"{operand}" is not op_a number')
 
 if __name__ == '__main__':
     c = Calculator()
@@ -40,18 +52,17 @@ if __name__ == '__main__':
         for i, operation in enumerate(operations, start=1):
             print(f"{i}: {operation.__name__}")
         print("q: quit")
-        
+
         operation = input("Pick an operation: ")
         if operation == "q":
             sys.exit()
         op = int(operation)
-        
-        a = float(input("a: "))
-        b = float(input("b: "))
-        
+
+        my_a = float(input("a: "))
+        my_b = float(input("b: "))
+
         try:
-            print(f"Result: {operations[op - 1](a, b)}")
+            print(f"Result: {operations[op - 1](my_a, my_b)}")
         except CalculatorError as err:
             print(err)
-            
             
--- notebooks/Pytest Example/test_calculator.py
+++ notebooks/Pytest Example/test_calculator.py
@@ -1,34 +1,43 @@
-from calculator import Calculator, CalculatorError
+"""Test the terrible calculator"""
+
 import pytest
+from calculator import Calculator, CalculatorError
 
 def test_add():
-    c = Calculator()
-    assert c.add(2,3) == 5
+    """test"""
+    calc = Calculator()
+    assert calc.add(2,3) == 5
 
 def test_add_weird_stuff():
-    c = Calculator()
+    """test"""
+    calc = Calculator()
     with pytest.raises(CalculatorError):
-        result = c.add("two",3)
+        calc.add("two",3)
 
 def test_add_weirder_stuff():
-    c = Calculator()
-    with pytest.raises(CalculatorError) as context:
-        result = c.add("two","three")
-    
+    """test"""
+    calc = Calculator()
+    with pytest.raises(CalculatorError):
+        calc.add("two","three")
+
 def test_subtract():
-    c = Calculator()
-    assert c.subtract(2,3) == -1
+    """test"""
+    calc = Calculator()
+    assert calc.subtract(2,3) == -1
 
 def test_multiply():
-    c = Calculator()
-    assert c.multiply(2,3) == 6
+    """test"""
+    calc = Calculator()
+    assert calc.multiply(2,3) == 6
 
 def test_divide():
-    c = Calculator()
-    assert c.divide(5,2) == 2.5
+    """test"""
+    calc = Calculator()
+    assert calc.divide(5,2) == 2.5
 
 def test_divide_by_zero():
-    c = Calculator()
+    """test"""
+    calc = Calculator()
     with pytest.raises(CalculatorError) as context:
-        result = c.divide(5,0)
-    assert str(context.value) = "Division by zero is invalid"
+        calc.divide(5,0)
+    assert str(context.value) == "Division by zero is invalid"
